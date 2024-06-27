5354ef14b0b26d4d03d4b5ccbb194ae49919cd90
ayushisingh22 <114730637+ayushisingh22@users.noreply.github.com>
2024-06-27 18:01:03 +0530

    Update Fift.java

--- ayushi/src/ayushi/Fift.java
+++ ayushi/src/ayushi/Fift.java
@@ -1,90 +1,90 @@
-//package ayushi;
-//
-//public class Fift {
-//
-//	public static void main(String[] args) {
-//		
-//		int a=10, b=20, c=5;
-//		
-//		System.out.println("The largest No is ");
-//		if(a>b) {
-//			if (a>c)
-//			System.out.println(a);
-//			else
-//				System.out.println(c);
-//		} else
-//			if (c>b)
-//				System.out.println(c);
-//			else 
-//				System.out.println(b);
-//		
-//		for (int i=1; i<=100; i++)
-//		{
-//			System.out.println("Value of i :" +i);
-//		}
-//		
-//		int i=10;
-//		while (i<=100)
-//		{
-//			System.out.println(i);
-//			i++;
-//		}
-//		int i=11;
-//		do {
-//			System.out.println(i);
-//			i++;
-//		}while(i<=1);
-//		int a[]= {10,20,30,40,50};
-//	
-//	for (int i : a)
-//	{
-//		System.out.println(i);
-//	}
-//	char ch[]= {'j','s', 'd','v'};
-//	for (char c : ch)
-//	{
-//		System.out.println(c);
-//	}
-//	String s1[]= {"Java,""apple","mango"}:
-//		for(String s: s1)
-//		{
-//			System.out.println(s1);
-//		}
-//	 int beg=10;
-//	 int end=20;
-//	  for(int i=beg; i<end; i++)
-//	  {
-//		  for (int j=1; j<=10; j++)
-//		  {
-//			  System.out.println(i + "");
-//		  }
-//	  }
-//	  char x='l';
-//	  switch(x)
-//	  {
-//	  case 'l':
-//	  case'L':
-//		  System.out.println(x+ "is a leader");
-//		  break;
-//	  case'd':
-//	  case'D':
-//		  System.out.println(x+"is digital");
-//		  break;
-//	  case's':
-//	  case'S':
-//		  System.out.println(x+ "is obstacle");
-//		  break;
-//		  
-//	      default:
-//			  System.out.println("default");
-//	}
-//		
-//		
-//		for(int k=5; k<15; k++)
-//		{
-//			if (k%2 !=0)
-//				continue;
-//			System.out.println(k);
-//		}
-//	}
+package ayushi;
+
+public class Fift {
+
+	public static void main(String[] args) {
+		
+		int a=10, b=20, c=5;
+		
+		System.out.println("The largest No is ");
+		if(a>b) {
+			if (a>c)
+			System.out.println(a);
+			else
+				System.out.println(c);
+		} else
+			if (c>b)
+				System.out.println(c);
+			else 
+				System.out.println(b);
+		
+		for (int i=1; i<=100; i++)
+		{
+			System.out.println("Value of i :" +i);
+		}
+		
+		int i=10;
+		while (i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+		int i=11;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<=1);
+		int a[]= {10,20,30,40,50};
+	
+	for (int i : a)
+	{
+		System.out.println(i);
+	}
+	char ch[]= {'j','s', 'd','v'};
+	for (char c : ch)
+	{
+		System.out.println(c);
+	}
+	String s1[]= {"Java,""apple","mango"}:
+		for(String s: s1)
+		{
+			System.out.println(s1);
+		}
+	 int beg=10;
+	 int end=20;
+	  for(int i=beg; i<end; i++)
+	  {
+		  for (int j=1; j<=10; j++)
+		  {
+			  System.out.println(i + "");
+		  }
+	  }
+	  char x='l';
+	  switch(x)
+	  {
+	  case 'l':
+	  case'L':
+		  System.out.println(x+ "is a leader");
+		  break;
+	  case'd':
+	  case'D':
+		  System.out.println(x+"is digital");
+		  break;
+	  case's':
+	  case'S':
+		  System.out.println(x+ "is obstacle");
+		  break;
+		  
+	      default:
+			  System.out.println("default");
+	}
+		
+		
+		for(int k=5; k<15; k++)
+		{
+			if (k%2 !=0)
+				continue;
+			System.out.println(k);
+		}
+	}
 }
